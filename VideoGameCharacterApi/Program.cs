228ab3763346ab781b4538d0a5076249c93b277f
Vlad Ciunel <vlad.ciunel@gmail.com>
2026-08-22 21:14:04 +0300

    Added DbContext

--- VideoGameCharacterApi/Program.cs
+++ VideoGameCharacterApi/Program.cs
@@ -1,13 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using VideoGameCharacterApi.Data;
 using VideoGameCharacterApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+(
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IVideoGameCharacterService, VideoGameCharacterService>();
 var app = builder.Build();
 
