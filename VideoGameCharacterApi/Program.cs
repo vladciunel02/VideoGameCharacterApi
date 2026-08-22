500256f4a9ac5350a560ab72f1bea1883e842e81
Vlad Ciunel <vlad.ciunel@gmail.com>
2026-08-22 21:48:14 +0300

    Created the DataBase and made changes to existing methods.

--- VideoGameCharacterApi/Program.cs
+++ VideoGameCharacterApi/Program.cs
@@ -6,7 +6,7 @@ using VideoGameCharacterApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-(
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
--- VideoGameCharacterApi/Services/VideoGameCharacterService.cs
+++ VideoGameCharacterApi/Services/VideoGameCharacterService.cs
@@ -1,8 +1,9 @@
 ﻿using VideoGameCharacterApi.Models;
-
+using VideoGameCharacterApi.Data;
+using Microsoft.EntityFrameworkCore;
 namespace VideoGameCharacterApi.Services
 {
-    public class VideoGameCharacterService : IVideoGameCharacterService
+    public class VideoGameCharacterService(AppDbContext context) : IVideoGameCharacterService
     {
         static List<Character> characters = new List<Character>
         {
@@ -23,12 +24,12 @@ namespace VideoGameCharacterApi.Services
 
         public async Task<Character?> GetCharacterByIdAsync(int characterId)
         {
-            var result = characters.FirstOrDefault(c => c.Id == characterId);
-            return await Task.FromResult(result);
+            var result = await context.Characters.FindAsync(characterId);
+            return result;
         }
 
-        public Task<List<Character>> GetAllCharactersAsync()
-            => Task.FromResult(characters);
+        public async Task<List<Character>> GetAllCharactersAsync()
+            => await context.Characters.ToListAsync();
 
         public Task<bool> UpdateCharacterAsync(int characterId, Character character)
         {
